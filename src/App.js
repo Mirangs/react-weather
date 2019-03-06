dc37f617437e219158b02da5965d8848c172cded
Mirangs <berezanskij98@gmail.com>
2019-03-06 13:17:54 +0200

    Fixed https bug

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     const country = evt.target.elements.country.value;
 
     if (city && country) {
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       const data = await api_call.json();
       this.setState({ 
         temperature: data.main.temp,
